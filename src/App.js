cc485600b7e79359245c83d59998dbf3dc486ba7
hoho715 <andy2001@g.hongik.ac.kr>
2025-08-21 17:55:57 +0900

    feat: 메인 홈페이지 생성

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import QsccLanding from './pages/QSCC/QsccLanding'
 import WhatIsQscc from './pages/QSCC/about/WhatIsQscc'
 import Test from './pages/QSCC/test/Test'
+import HomePage from './pages/home/HomePage'
 
 
 export default function App() {
   return (
     <Routes>
+
+      <Route path="/" element={<HomePage />} />
+
       <Route path="/qscc" element={<QsccLanding />} />
       <Route path="/qscc/whatisqscc" element={<WhatIsQscc />} />
       <Route path="/qscc/test" element={<Test />} />
       
-      <Route path="/" element={<Navigate to="/qscc" replace />} />
+      <Route path="/test" element={<Navigate to="/qscc" replace />} />
     </Routes>
   )
 }
---
+++ src/pages/home/HomePage.js
@@ -0,0 +1,16 @@
+// import "../../styles/HomePage.css";
+import Header from '../../components/layouts/Header'
+
+function HomePage() {
+  return (
+    
+    <div>
+              <Header />
+        
+      <h1>"이음; 나의 체질과 하루를 잇다"</h1>
+      <p>당신의 체질을 분석해 건강한 하루를 설계합니다</p>
+    </div>
+  );
+}
+
+export default HomePage;
