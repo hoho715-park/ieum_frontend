69609df3805af11f5313b4c5ab40350cbb38415f
hoho715 <andy2001@g.hongik.ac.kr>
2025-08-22 10:57:19 +0900

    feat(layout): Layout.js에 Footer 컴포넌트 추가
    - 페이지 하단에 <Footer /> 연결

--- src/components/layouts/Layout.js
+++ src/components/layouts/Layout.js
@@ -1,17 +1,18 @@
 import Header from "./Header";
+import Footer from "./Footer";
 
 function Layout({ children }) {
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
 
-            <main className="flex-grow overflow-y-auto">
-                <div className="container mx-auto px-4 py-8">
+            <main className="overflow-y-auto">
+                <div className="container mx-auto px-4 py-8 mb-8">
                     {children}
                 </div>
             </main>
 
-            
+           <Footer /> 
         </div>
     );
 };
